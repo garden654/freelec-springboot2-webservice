6c4b52462dd476db603a63b5add7eadfc5e93e3c
garden654 <gardensdv33@gmail.com>
2022-06-18 23:03:15 +0900

    Posts 엔티티 비교 수정

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -5,19 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
-@NoArgsConstructor
-@Entity
+@NoArgsConstructor  // 기본 생성자 생성.
+@Entity  // JPA ANNOTATION. 테이블과 링크될 어노테이션
 public class Posts extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id     // 테이블의 PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // PK 생성규칙. auto_increment
     private Long id;
 
     @Column(length = 500, nullable = false)
@@ -28,16 +24,15 @@ public class Posts extends BaseTimeEntity {
 
     private String author;
 
-    @Builder
-    public Posts(String title, String content, String author) {
+    @Builder    // 해당 클래스의 빌드 패턴 생성. 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author){
         this.title = title;
         this.content = content;
         this.author = author;
     }
 
-    public void update(String title, String content) {
-        this.title  = title;
+    public void update(String title, String content){
+        this.title = title;
         this.content = content;
     }
-
 }
