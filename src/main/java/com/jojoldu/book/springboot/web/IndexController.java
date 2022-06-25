ce385e0ceea88150b3914653f9e94940d708690e
garden654 <gardensdv33@gmail.com>
2022-06-25 17:47:22 +0900

    원상복구!

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -6,9 +6,8 @@ import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
@@ -16,30 +15,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
-
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user) {
-        model.addAttribute("posts", postsService.
-findAllDesc());
-        if (user != null) {
+    public String index(Model model, @LoginUser SessionUser user){
+        // model: 서버 템플릿 엔진에서 사용할 수 있는 개체 저장 가능.
+        // 이제 어느 컨트롤러든지 @LogintUser만 사용하면 세션 정보 가져올 수 있다.
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null){
             model.addAttribute("userName", user.getName());
-        }
+        }  // 세션에 저장된 값이 있을때만 모델에 userName 등록
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave() {
+    public String postsSave(){
         return "posts-save";
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-
+    public String postsUpdate(@PathVariable Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
         return "posts-update";
     }
+
 }
+
 
 
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -9,24 +9,25 @@ var main = {
         });
         $('#btn-delete').on('click', function (){
             _this.delete();
-        });
+        })
     },
-    save : function () {
+    save : function (){
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
             content: $('#content').val()
         };
+
         $.ajax({
-            type: 'POST',
+            type: 'POST',    // 생성(Create)
             url: '/api/v1/posts',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function (){
             alert('글이 등록되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
+            window.location.href = '/';    // 글 등록이 성공하면 메인페이지(/)로 이동합니다.
+        }).fail(function (error){
             alert(JSON.stringify(error));
         });
     },
@@ -39,15 +40,15 @@ var main = {
         var id = $('#id').val();
 
         $.ajax({
-            type: 'PUT',
+            type: 'PUT',   // 수정(Update)
             url: '/api/v1/posts/'+id,
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function (){
             alert('글이 수정되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
+            window.location.href = '/';    // 글 등록이 성공하면 메인페이지(/)로 이동합니다.
+        }).fail(function (error){
             alert(JSON.stringify(error));
         });
     },
@@ -55,17 +56,17 @@ var main = {
         var id = $('#id').val();
 
         $.ajax({
-            type: 'DELETE',
+            type: 'DELETE',   // 삭제(DELETE)
             url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8'
-        }).done(function () {
+            contentType: 'application/json; charset=utf-8',
+        }).done(function (){
             alert('글이 삭제되었습니다.');
             window.location.href = '/';
-        }).fail(function (error) {
+        }).fail(function (error){
             alert(JSON.stringify(error));
         });
     }
-};
 
+};
 main.init();
